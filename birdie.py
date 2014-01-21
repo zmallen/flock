5a231fa1bcc352400d059fbb1d2d5dd8e67133ed
Zack Allen <zack@riskive.com>
2014-01-21 10:47:04 -0500

    bot can now access irc and mirror messages. need some type of authentication. need to set up campaign structure, either we run crontabs to send shit to irc, or we specify every 6 hours.

--- birdie.py
+++ birdie.py
@@ -2,6 +2,7 @@
 import gevent
 from twython import Twython, TwythonError
 from ircbot import IrcNodeHead
+from ircbot2 import TestBot
 
 class TwitterBot:
     def __init__(self, name, con_k, con_s, acc_k, acc_s):
@@ -22,36 +23,38 @@ class TwitterBot:
 def main():
     # get bot info
     with open('bots.csv', 'r') as bot_file:
-        try:
-            read_in = bot_file.readlines()
-            # get lines and filter out comments or misconfigured lines
-            lines = [ l.rstrip() for l in read_in if not l.startswith("#") and (l.startswith("bot=") or l.startswith("irc="))]
-            if len(lines) <= 0:
-               raise Exception("Could not load any bots from bot_file") 
-            # read in irc info, will always read first one
-            irc_lines = [ l for l in lines if l.startswith("irc=")]
-            if len(irc_lines) <= 0:
-                raise Exception("Could not load IRC info from bot_file")
-            # remove irc_line from lines
-            map(lambda x: lines.remove(x), irc_lines)
-            print irc_lines
-            irc_serv, irc_chan = irc_lines[0].split("=")[1].split(",")
-            # spawn the bots, give them 45 seconds to connect to twitter then return the object 
-            # we will wait and start the thread for irc as our main event loop
-            jobs = [ gevent.spawn(spawn_bots, line) for line in lines ]
-            gevent.joinall(jobs, timeout=45)
-            # get all the twitter bots, will raise exception if oauth fails
-            bot_list = [ bot.value for bot in jobs ]
-            # join irc as a nodehead , this bot controls many twitter bots and runs campaigns for all
-            port = "6667"
-            if ":" in irc_serv:
-                irc_serv,port = irc_serv.split(":")
-            irc_bot = IrcNodeHead(irc_chan, name, irc_serv, port)
-            irc_bot.start()
+        #try:
+        read_in = bot_file.readlines()
+        # get lines and filter out comments or misconfigured lines
+        lines = [ l.rstrip() for l in read_in if not l.startswith("#") and (l.startswith("bot=") or l.startswith("irc="))]
+        if len(lines) <= 0:
+           raise Exception("Could not load any bots from bot_file") 
+        # read in irc info, will always read first one
+        irc_lines = [ l for l in lines if l.startswith("irc=")]
+        if len(irc_lines) <= 0:
+            raise Exception("Could not load IRC info from bot_file")
+        # remove irc_line from lines
+        map(lambda x: lines.remove(x), irc_lines)
+        print irc_lines
+        irc_serv, irc_chan = irc_lines[0].split("=")[1].split(",")
+        # spawn the bots, give them 45 seconds to connect to twitter then return the object 
+        # we will wait and start the thread for irc as our main event loop
+        jobs = [ gevent.spawn(spawn_bots, line) for line in lines ]
+        gevent.joinall(jobs, timeout=45)
+        # get all the twitter bots, will raise exception if oauth fails
+        bot_list = [ bot.value for bot in jobs ]
+        # join irc as a nodehead , this bot controls many twitter bots and runs campaigns for all
+        port = 6667
+        if ":" in irc_serv:
+            print "in if: " + irc_serv
+            irc_serv, port = irc_serv.split(":")
+        irc_bot = IrcNodeHead(irc_chan, "test", irc_serv, int(port))
+        irc_bot.start()
+        print irc_chan + " " + irc_serv + " " + str(port)
 
-        except Exception as e:
-            #syslog.syslog(syslog.LOG_ERR, 'BIRDIE: ' + str(e))
-            print "ERROR: " + str(e)
+    #except Exception as e:
+        #syslog.syslog(syslog.LOG_ERR, 'BIRDIE: ' + str(e))
+        #print "ERROR: " + str(e)
 
 # spawn bots given the config options, and return the object to jobs
 def spawn_bots(bot_line):
--- ircbot.py
+++ ircbot.py
@@ -5,6 +5,7 @@ from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 class IrcNodeHead(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
 
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
@@ -16,7 +17,5 @@ class IrcNodeHead(irc.bot.SingleServerIRCBot):
         self.do_command(e, e.arguments[0])
 
     def on_pubmsg(self, c, e):
-        a = e.arguments[0].split(":", 1)
-        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
-            self.do_command(e, a[1].strip())
-        return
+        a = e.arguments[0]
+        c.privmsg(self.channel, "you said: " + a)
